26f61575e020fd2a386d69854b8611171b37b7e8
connor@colorad.io <connor.stricklan@gmail.com>
2018-01-05 10:22:54 -0700

    Tune Speech systhesis in js port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,23 @@ require("./styles.scss");
 
 var Elm = require('./Main');
 var app = Elm.Main.fullscreen();
+
 var synth = window.speechSynthesis;
+var wordPending = '';
 app.ports.speak.subscribe(function(word) {
-  synth.speak(new SpeechSynthesisUtterance(word));
+  var wasSpeaking = synth.speaking;
+  console.log(wasSpeaking, wordPending, (new Date()).getMilliseconds(), word);
+  if (!wasSpeaking && !wordPending) {
+    synth.speak(new SpeechSynthesisUtterance(word));
+    wordPending = word;
+    setTimeout(function () { wordPending = ''; }, 500);
+  } else {
+    if (wordPending === word) return;
+    if (wasSpeaking) synth.cancel();
+    wordPending = word;
+    setTimeout(function () {
+      synth.speak(new SpeechSynthesisUtterance(word));
+      wordPending = '';
+    },500);
+  }
 });
